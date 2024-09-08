0f4fb8d5c3a30e971dbe91d3298053bc6a2d1389
Timur Shamsrakhmanov <t.shamsrakhmanov@gmail.com>
2024-09-08 09:16:58 +0400

    general errors (kafka timeout especially) handled.

--- main.py
+++ main.py
@@ -2,11 +2,23 @@ from fastapi import FastAPI, HTTPException
 from confluent_kafka import Producer, Consumer, KafkaException
 from datetime import datetime
 from argparse import ArgumentParser
+import logging
 
 
-def make_fastapi_application(broker_socket):
+def make_fastapi_application(broker_socket) -> FastAPI:
     # create instance of an app
     app = FastAPI()
+
+    # function of callback an error, must be included in CONFIG down below
+    def error_cb(err):
+        # TODO change to logging system
+        print('[ERROR] Callback error:', err)
+
+    config_producer = {'bootstrap.servers': f'{broker_socket}',
+                       'error_cb': error_cb,
+                       'message.timeout.ms': 10
+                       }
+    producer = Producer(config_producer)
 
     # simple get handle
     @app.get("/about")
@@ -16,25 +28,26 @@ def make_fastapi_application(broker_socket):
     # handler to send message to kafka
     @app.post("/to-kafka-datetime")
     async def get_one(message: str):
-        print(f'[INFO] Message recieved:', message)
 
-        print('           BEFORE 1 TRY')
+        def delivery_report(err, msg):
+            """ Called once for each message produced to indicate delivery result.
+                Triggered by poll() or flush(). """
+            if err is not None:
+                # TODO change to logging system
+                print('[ERROR] Delivery report error: {}'.format(err))
+                raise HTTPException(500, f"{err}")
+            else:
+                # TODO change to logging system
+                print('[SUCCESS] Delivery report: {} [{}]'.format(msg.topic(), msg.partition()))
 
-        try:
-            config = {'bootstrap.servers': f'{broker_socket}'}
-            producer = Producer(config)
-            producer.produce('test1', value=f'<<<{datetime.now()}>>>, {message}')
-        except KafkaException as e:
-            raise HTTPException(502, f"Kafka error: {e}")
+        producer.produce('test1', value=f'<<<{datetime.now()}>>>, {message}', callback=delivery_report)
 
-        print('           BEFORE 2 TRY')
+        # any time flush is called - callback from *produce* (up above)  will be called
+        # if a callback will contain some errors - they will be handled inside delivery report
+        producer.flush(timeout=1.0)
 
-        try:
-            producer.flush(timeout=1.0)
-        except KafkaException as e:
-            print(f'[ERROR] Error occured: {e.__class__}, {e.__class__.__name__}')
-            raise HTTPException(502, f"Kafka error: {e}")
 
+    # TODO add get from KAFKA handle
     # return app with all handlers to run it in uvicorn server
     return app
 
